7aeb4bdfdd42eab2fe78093857570b7b49ee597f
NguyenCatNguyen <catngn11@gmail.com>
2024-02-28 11:23:40 -0600

    update

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Zelda-Wiki
- 
+# Zelda Compendium
+
+## Live Site
+
+
+## Why I Build This
+
+## Technologies Used
+
+## Features
+
+## What I Learned
+### 1 Learn how to use API to fetch data from the server
+- I learn that the data I receive from the server is in a object format, so I need to convert it to an array to use the array methods.
+```javascript
+// Varible to access the entries list
+const entryList = document.querySelector(".entries__lists")
+
+async function main(){
+    const entries = (await fetch("https://botw-compendium.herokuapp.com/api/v3/compendium/all"));
+    const entriesData = await entries.json();
+    
+    //Access the array
+    const entriesArray = entriesData.data.sort((a,b)=>  a.id - b.id); //Sort  array in order
+    entryList.innerHTML = entriesArray.map((entry)=>itemHTML(entry)).join("");
+}
+```
+
+### 2. Sort an array of objects by a property value
+```javascript
+const sortedData = data.sort((a, b) => a.name.localeCompare(b.name));
+```
+
+### 3. How to make category button more dynamic and reusable
+- Set the button to be able to change the category when clicked and also remain active
+- Be able to select and unselect the button.
+- There are two case when button is unselected:
+    - Another button is being selected and cause the current button to be unselected.
+    - The current button is being unselected and the full data is being displayed. 
+
+
---
+++ script.js
@@ -0,0 +1,75 @@
+/*
+- Get all entries: https://botw-compendium.herokuapp.com/api/v3/compendium/all
+- Get category entries: https://botw-compendium.herokuapp.com/api/v3/compendium/category/<category>
+
+*/
+
+// Initial variable
+const entryList = document.querySelector(".entries__lists")
+
+// Get entry data
+function itemHTML(entry){
+    return`
+    <div class="entries__card">
+                    <img src="${entry.image}"" alt="" class="entry__img">
+                    <div class="entry__info">
+                        <p class="entry__id">${entry.id}</p>
+                        <h3 class="entry__name">
+                            ${entry.name}
+                        </h3>
+                    </div>
+                </div>
+    `
+}
+
+
+async function main(){
+    const entries = (await fetch("https://botw-compendium.herokuapp.com/api/v3/compendium/all"));
+    const entriesData = await entries.json(); //Convert the data to JSON
+    // Access the data and sort it base on ID
+    const entriesArray = entriesData.data.sort((a,b)=>  a.id - b.id);
+    entryList.innerHTML = entriesArray.map((entry)=>itemHTML(entry)).join("");
+    
+}
+
+
+const category_list = document.querySelectorAll(".category")
+// Fetch Category button 
+async function FetchCategory(category){
+    const entries = await fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/${category}`)
+    const entriesData = await entries.json();
+
+    // Access the data and sort it base on ID
+    const entriesArray = entriesData.data.sort((a,b)=>  a.id - b.id); //Sort  array in order
+    entryList.innerHTML = entriesArray.map((entry)=>itemHTML(entry)).join("");
+}
+
+// Loading screen
+function showLoadingScreen(element) {
+    // Add loading class to the element
+    let elementClass = querySelector(`.${element}`);
+    elementClass.classList.add('loading');
+
+    // Remove loading class after the specified duration
+    setTimeout(() => element.classList.remove('loading'), 2000);
+}
+
+// Get start button 
+async function start_function(){
+    document.querySelector(".landing").classList.add("hidden");
+    await main(); // Wait for main function to finish retrieving data.
+    document.querySelector(".category__List").classList.add("appear");
+    document.querySelector(".nav_list").classList.add("appear");
+    document.querySelector(".entries").classList.add("visible");
+}
+
+// Search Bar Function
+
+// Run the method
+document.getElementById('equipmentButton').addEventListener('click', () => FetchCategory('equipment'));
+document.getElementById('treasureButton').addEventListener('click', () => FetchCategory('treasure'));
+document.getElementById('materialsButton').addEventListener('click', () => FetchCategory('materials'));
+document.getElementById('creaturesButton').addEventListener('click', () => FetchCategory('creatures'));
+document.getElementById('monstersButton').addEventListener('click', () => FetchCategory('monsters'));
+
+
